0883ddaa4f41e5df36d78479a967f54782288b28
Tom Gilligan <thomas.gilligan@icloud.com>
2021-06-09 18:55:28 +1000

    no print styles

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -166,7 +166,7 @@ module ActiveAdmin
 
     def register_default_assets
       register_stylesheet "active_admin.css", media: "screen"
-      register_stylesheet "active_admin/print.css", media: "print"
+      # register_stylesheet "active_admin/print.css", media: "print"
       register_javascript "active_admin.js"
     end
 
